4039fbac22c213b5be4d1c2930cc27a5481f23eb
Bitterlox <AngelloxBit@gmail.com>
2018-08-21 04:37:29 +0200

    Logging

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
-	"github.com/rs/cors"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	. "github.com/ubiq/spectrum-api/config"
 	. "github.com/ubiq/spectrum-api/dao"
 	. "github.com/ubiq/spectrum-api/models"
@@ -18,54 +20,55 @@ var dao_ = SpectrumDAO{}
 var port string
 
 type AccountTxn struct {
-	Txns            []Transaction  `bson:"txns" json:"txns"`
-	Total           int             `bson:"total" json:"total"`
+	Txns  []Transaction `bson:"txns" json:"txns"`
+	Total int           `bson:"total" json:"total"`
 }
 
 type AccountTokenTransfer struct {
-	Txns            []TokenTransfer `bson:"txns" json:"txns"`
-	Total           int             `bson:"total" json:"total"`
+	Txns  []TokenTransfer `bson:"txns" json:"txns"`
+	Total int             `bson:"total" json:"total"`
 }
 
 func getBlockByHash(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	block, err := dao_.BlockByHash(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
+		respondWithError(w, r, http.StatusBadRequest, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, block)
+
+	respondWithJson(w, r, http.StatusOK, block)
 }
 
 func getBlockByNumber(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	number, uerr := strconv.ParseUint(params["number"], 10, 64)
 	if uerr != nil {
-		respondWithError(w, http.StatusBadRequest, uerr.Error())
+		respondWithError(w, r, http.StatusBadRequest, uerr.Error())
 		return
 	}
 	block, err := dao_.BlockByNumber(number)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
+		respondWithError(w, r, http.StatusBadRequest, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, block)
+	respondWithJson(w, r, http.StatusOK, block)
 }
 
 func getLatestBlock(w http.ResponseWriter, r *http.Request) {
 	blocks, err := dao_.LatestBlock()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, blocks)
+	respondWithJson(w, r, http.StatusOK, blocks)
 }
 
 func getLatestBlocks(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	limit, err := strconv.Atoi(params["limit"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if limit > 1000 {
@@ -73,17 +76,17 @@ func getLatestBlocks(w http.ResponseWriter, r *http.Request) {
 	}
 	blocks, err := dao_.LatestBlocks(limit)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, blocks)
+	respondWithJson(w, r, http.StatusOK, blocks)
 }
 
 func getLatestForkedBlocks(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	limit, err := strconv.Atoi(params["limit"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if limit > 1000 {
@@ -91,17 +94,17 @@ func getLatestForkedBlocks(w http.ResponseWriter, r *http.Request) {
 	}
 	blocks, err := dao_.LatestForkedBlocks(limit)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, blocks)
+	respondWithJson(w, r, http.StatusOK, blocks)
 }
 
 func getLatestTransactions(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	limit, err := strconv.Atoi(params["limit"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if limit > 1000 {
@@ -109,12 +112,12 @@ func getLatestTransactions(w http.ResponseWriter, r *http.Request) {
 	}
 	txns, err := dao_.LatestTransactions(limit)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	count, err := dao_.TotalTxnCount()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 
@@ -122,19 +125,19 @@ func getLatestTransactions(w http.ResponseWriter, r *http.Request) {
 	res.Txns = txns
 	res.Total = count
 
-	respondWithJson(w, http.StatusOK, res)
+	respondWithJson(w, r, http.StatusOK, res)
 }
 
 func getLatestTransactionsByAccount(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	txns, err := dao_.LatestTransactionsByAccount(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	count, err := dao_.TxnCount(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 
@@ -142,20 +145,20 @@ func getLatestTransactionsByAccount(w http.ResponseWriter, r *http.Request) {
 	res.Txns = txns
 	res.Total = count
 
-	respondWithJson(w, http.StatusOK, res)
+	respondWithJson(w, r, http.StatusOK, res)
 }
 
 func getLatestTokenTransfersByAccount(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	txns, err := dao_.LatestTokenTransfersByAccount(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	count, err := dao_.TokenTransferCount(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 
@@ -163,14 +166,14 @@ func getLatestTokenTransfersByAccount(w http.ResponseWriter, r *http.Request) {
 	res.Txns = txns
 	res.Total = count
 
-	respondWithJson(w, http.StatusOK, res)
+	respondWithJson(w, r, http.StatusOK, res)
 }
 
 func getLatestTokenTransfers(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	limit, err := strconv.Atoi(params["limit"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if limit > 1000 {
@@ -178,17 +181,17 @@ func getLatestTokenTransfers(w http.ResponseWriter, r *http.Request) {
 	}
 	transfers, err := dao_.LatestTokenTransfers(limit)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, transfers)
+	respondWithJson(w, r, http.StatusOK, transfers)
 }
 
 func getLatestUncles(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	limit, err := strconv.Atoi(params["limit"])
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
 	if limit > 1000 {
@@ -196,47 +199,65 @@ func getLatestUncles(w http.ResponseWriter, r *http.Request) {
 	}
 	uncles, err := dao_.LatestUncles(limit)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		respondWithError(w, r, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, uncles)
+	respondWithJson(w, r, http.StatusOK, uncles)
 }
 
 func getTransactionByHash(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	txn, err := dao_.TransactionByHash(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusOK, err.Error())
+		respondWithError(w, r, http.StatusOK, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, txn)
+	respondWithJson(w, r, http.StatusOK, txn)
 }
 
 func getUncleByHash(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	uncle, err := dao_.UncleByHash(params["hash"])
 	if err != nil {
-		respondWithError(w, http.StatusOK, err.Error())
+		respondWithError(w, r, http.StatusOK, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, uncle)
+	respondWithJson(w, r, http.StatusOK, uncle)
 }
 
 func getStore(w http.ResponseWriter, r *http.Request) {
 	store, err := dao_.Store()
 	if err != nil {
-		respondWithError(w, http.StatusOK, err.Error())
+		respondWithError(w, r, http.StatusOK, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, store)
+	respondWithJson(w, r, http.StatusOK, store)
 }
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
+func respondWithError(w http.ResponseWriter, r *http.Request, code int, msg string) {
+
+	log.WithFields(log.Fields{
+		"path":  r.URL,
+		"ip":    r.RemoteAddr,
+		"error": msg,
+	}).Error()
+
+	respondWithJson(w, r, code, map[string]string{"error": msg})
 }
 
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
+func respondWithJson(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+
+	response, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Errorf("Could not marshal response: %v", err)
+	}
+
+	log.WithFields(log.Fields{
+		"path": r.URL,
+		"ip":   r.RemoteAddr,
+	}).Info()
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
@@ -251,7 +272,15 @@ func init() {
 	dao_.Connect()
 }
 
+func init() {
+	log.SetFormatter(&log.TextFormatter{DisableLevelTruncation: true, FullTimestamp: true, TimestampFormat: time.RFC822})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
+
 func main() {
+	log.Info("Api started on port ", port)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/status", getStore).Methods("GET")
 	r.HandleFunc("/block/{number}", getBlockByNumber).Methods("GET")
